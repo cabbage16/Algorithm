e636d35a74bed41d4b4158085886f3ac59960f67
cabbage16 <baejaehyeon0106@gmail.com>
2023-09-25 11:07:03 +0900

    계수 정렬

---
+++ sort/counting-sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int n, t, arr[100000] = {};
+	int min = 4500000, max = 0;
+	scanf("%d", &n);
+
+	while(n--) {
+		scanf("%d", &t);
+		arr[t]++;
+		if(t > max) max = t;
+		if(t < min) min = t;
+	}
+
+	for(int i = min; i <= max; i++) {
+		for(int j = 0; j < arr[i]; j++)
+			printf("%d ", i);
+	}
+
+	return 0;
+}
